2fdf098fe79dfdd004a08aafe628d3aa02a7d328
DaniilLysyy <don.dmitro2013@ya.ru>
2026-06-08 16:49:41 +0300

    first_draft

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,34 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
-// @todo: #4.3 — настроить компаратор
-
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes)
+    .forEach((elementName) => {
+      elements[elementName].append(
+        ...Object.values(indexes[elementName])
+          .map(name => {
+            const option = document.createElement('option');
+            option.value = name;
+            option.textContent = name;
+            return option;
+          })
+      );
+    });
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+  return (data, state, action) => {
+    if (action?.name === 'clear') {
+      const button = elements.clear;
+      const parent = button.closest('.filter__field');
+      const input = parent.querySelector('input');
+      const field = button.getAttribute('data-field');
+      
+      if (input && field) {
+        input.value = '';
+        state[field] = '';
+      }
     }
+
+    const compare = createComparison(defaultRules);
+
+    return data.filter(row => compare(row, state));
+  };
 }
